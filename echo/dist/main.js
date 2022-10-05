1b812791fdca49b604ab0e270316fbfd2581ebef
William Giovanni Cheverria Pacheco <cheverriawg@gmail.com>
2022-10-05 00:24:41 -0600

    finalizando echo

--- echo/dist/main.js
+++ echo/dist/main.js
@@ -1,3 +1,14 @@
 "use strict";
-console.log("Comando Echo");
-var mensaje = "";
+//modulo = require("modulo")
+var readline = require('readline');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question("echo ", function (mensaje) {
+    //eliminar las comillas dobles y simples
+    mensaje = mensaje.split('"').join('');
+    mensaje = mensaje.split("'").join('');
+    console.info("".concat(mensaje));
+    rl.close();
+});
--- echo/src/main.ts
+++ echo/src/main.ts
@@ -1,3 +1,15 @@
-console.log("Comando Echo");
-let mensaje:string = "";
+//modulo = require("modulo")
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+rl.question("echo ", function (mensaje:string) { 
+    //eliminar las comillas dobles y simples
+    mensaje = mensaje.split('"').join(''); 
+    mensaje = mensaje.split("'").join('');
+    
+	console.info(`${mensaje}`);
+	rl.close();
+})
