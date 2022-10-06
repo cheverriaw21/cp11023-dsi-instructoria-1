214498e4e679dd8508bdba5c5ce395ccb235b280
William Giovanni Cheverria Pacheco <cheverriawg@gmail.com>
2022-10-06 17:58:52 -0600

    utilizando copyfile

--- cp/dist/main.js
+++ cp/dist/main.js
@@ -2,6 +2,7 @@
 var readline = require('readline');
 //para leer archivo
 var fs = require("fs");
+var COPYFILE_EXCL = fs.constants.COPYFILE_EXCL;
 //ruta o entrada
 var rl = readline.createInterface({
     input: process.stdin,
@@ -11,21 +12,33 @@ var rl = readline.createInterface({
 rl.question("cp ", function (entrada) {
     var rutas = entrada.split(" ", 2);
     console.info(rutas);
+    fs.copyFile("./" + rutas[0], "." + rutas[1] + "/copia-" + rutas[0], COPYFILE_EXCL, function (err) {
+        if (err)
+            throw ("¡Entrada Invalida!\n" + err);
+        console.log("Archivo copiado...1");
+        // fs.rename("./"+"copia-"+rutas[0],"."+rutas[1]+"/copia-"+rutas[0],(err:any)=>{
+        //     if (err) throw ("¡Ha ocurrido un error!"+err);
+        //     console.log("Archivo copiado...2")
+        // });
+    });
+    /*
     //leer archivo
-    fs.readFile(rutas[0], 'UTF-8', function (error, archivo) {
-        if (error) {
+    fs.readFile(rutas[0],'UTF-8',(error:any,archivo:any) =>{
+        if(error){
             console.error("¡Entrada no válida, comprueba que sea un archivo!");
         }
         //si esta definido se muestra
-        if (archivo) {
+        if(archivo){
             console.info(archivo);
-            fs.write("copia", archivo, function (err) {
-                if (err) {
-                    throw ("Hubo un error");
+            fs.writeFile("copia",archivo,(err:any)=>{
+                if(err){
+                    throw("Hubo un error");
                 }
-                console.info("Archivo Copiado");
+                console.info("Archivo Copiado")
+
             });
         }
     });
+    */
     rl.close();
 });
--- cp/src/main.ts
+++ cp/src/main.ts
@@ -2,6 +2,7 @@ const readline = require('readline');
 
 //para leer archivo
 let fs = require("fs");
+const {COPYFILE_EXCL} = fs.constants;
 
 //ruta o entrada
 const rl = readline.createInterface({
@@ -14,6 +15,19 @@ rl.question("cp ", function (entrada:string) {
     let rutas:string[] = entrada.split(" ",2)
 
     console.info(rutas)
+    fs.copyFile(rutas[0],"."+rutas[1]+"/copia-"+rutas[0],COPYFILE_EXCL,(err:any)=>{
+        if (err) throw ("¡Entrada Invalida!\n"+err);
+        console.log("Archivo copiado...1")
+
+        // fs.rename("./"+"copia-"+rutas[0],"."+rutas[1]+"/copia-"+rutas[0],(err:any)=>{
+        //     if (err) throw ("¡Ha ocurrido un error!"+err);
+        //     console.log("Archivo copiado...2")
+        // });
+    
+    });
+
+  
+    /*
     //leer archivo
     fs.readFile(rutas[0],'UTF-8',(error:any,archivo:any) =>{
         if(error){
@@ -22,7 +36,7 @@ rl.question("cp ", function (entrada:string) {
         //si esta definido se muestra
         if(archivo){
             console.info(archivo);
-            fs.write("copia",archivo,(err:any)=>{
+            fs.writeFile("copia",archivo,(err:any)=>{
                 if(err){
                     throw("Hubo un error");
                 }
@@ -31,6 +45,7 @@ rl.question("cp ", function (entrada:string) {
             });
         }
     });
+    */
           
     rl.close();
 });
