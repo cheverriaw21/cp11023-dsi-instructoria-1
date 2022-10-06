56af344ec6a20d36b5b03c77e636b643547a6745
William Giovanni Cheverria Pacheco <cheverriawg@gmail.com>
2022-10-05 22:35:02 -0600

    trabajando entrada rutas y lectura escritura de archivo

--- cp/dist/main.js
+++ cp/dist/main.js
@@ -1,2 +1,31 @@
 "use strict";
-console.info("comando cp");
+var readline = require('readline');
+//para leer archivo
+var fs = require("fs");
+//ruta o entrada
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+//para leer la ruta
+rl.question("cp ", function (entrada) {
+    var rutas = entrada.split(" ", 2);
+    console.info(rutas);
+    //leer archivo
+    fs.readFile(rutas[0], 'UTF-8', function (error, archivo) {
+        if (error) {
+            console.error("¡Entrada no válida, comprueba que sea un archivo!");
+        }
+        //si esta definido se muestra
+        if (archivo) {
+            console.info(archivo);
+            fs.write("copia", archivo, function (err) {
+                if (err) {
+                    throw ("Hubo un error");
+                }
+                console.info("Archivo Copiado");
+            });
+        }
+    });
+    rl.close();
+});
--- cp/src/main.ts
+++ cp/src/main.ts
@@ -1,1 +1,36 @@
-console.info("comando cp")
+const readline = require('readline');
+
+//para leer archivo
+let fs = require("fs");
+
+//ruta o entrada
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+    
+//para leer la ruta
+rl.question("cp ", function (entrada:string) { 
+    let rutas:string[] = entrada.split(" ",2)
+
+    console.info(rutas)
+    //leer archivo
+    fs.readFile(rutas[0],'UTF-8',(error:any,archivo:any) =>{
+        if(error){
+            console.error("¡Entrada no válida, comprueba que sea un archivo!");
+        }
+        //si esta definido se muestra
+        if(archivo){
+            console.info(archivo);
+            fs.write("copia",archivo,(err:any)=>{
+                if(err){
+                    throw("Hubo un error");
+                }
+                console.info("Archivo Copiado")
+
+            });
+        }
+    });
+          
+    rl.close();
+});
